947f3191eabb20af550c8c2fd16a2200007c0e92
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-08-24 02:17:39 -0300

    :ok_hand: styles(index.ts): Applied linting

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,21 @@
-import 'dotenv/config'
-import express from 'express'
-import morgan from 'morgan';
-import cors from 'cors'
-import { address } from 'ip'
+import "dotenv/config";
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import { address } from "ip";
 
 const app = express();
-const port = process.env.PORT || 3000
-const protocol = app.settings['trust proxy'] ? 'https' : 'http'
+const port = process.env.PORT || 3000;
+const protocol = app.settings["trust proxy"] ? "https" : "http";
 
-app.use(morgan('dev'))
-app.use(cors())
-app.use(express.json())
-app.get(`/`, (req, res) => { res.status(200).json({ message: `Application running on ${protocol}://${req.hostname}:${port}` }) })
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
+app.get("/", (req, res) => { res.status(200).json({ message: `Application running on ${protocol}://${req.hostname}:${port}` }); });
 
-const server = app.listen(port, () => { console.log(`\nApplication running on ${protocol}://${address()}:${port}\n`) })
+const server = app.listen(port, () => { console.log(`\nApplication running on ${protocol}://${address()}:${port}\n`); });
 
-process.on('SIGINT', () => {
-    server.close()
-    console.log(`\nApplication terminated\n`)
-})
+process.on("SIGINT", () => {
+	server.close();
+	console.log("\nApplication terminated\n");
+});
