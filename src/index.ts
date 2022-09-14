314df2f7094801a323a235c118b6d543b84eccac
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-09-14 00:07:02 -0300

    :sparkles: feat(index.ts): add graceful shutdown

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import { address } from "ip";
 
 const app = express();
 const port = process.env.PORT || 3000;
+const processId = process.pid;
 const protocol = app.settings["trust proxy"] ? "https" : "http";
 
 app.use(morgan("dev"));
@@ -15,7 +16,22 @@ app.get("/", (req, res) => { res.status(200).json({ message: `Application runnin
 
 const server = app.listen(port, () => { console.log(`\nApplication running on ${protocol}://${address()}:${port}\n`); });
 
-process.on("SIGINT", () => {
-	server.close();
-	console.log("\nApplication terminated\n");
+process.on("SIGINT", (signal) => {
+	console.log(`APP (PID:${processId}) received ${signal}`);
+	server.close(() => { process.exit(0); });
+});
+process.on("SIGTERM", (signal) => {
+	console.log(`APP (PID:${processId}) received ${signal}`);
+	server.close(() => { process.exit(0); });
+});
+
+process.on("uncaughtException", (error, origin) => {
+	console.log(`APP (PID:${processId}) received ${origin}\n${error}`);
+	server.close(() => { process.exit(1); });
+	setTimeout(() => { process.exit(1); }, 1000).unref();
+});
+process.on("uncaughtException", (reason) => {
+	console.log(`APP (PID:${processId}) received ${reason}`);
+	server.close(() => { process.exit(1); });
+	setTimeout(() => { process.exit(1); }, 1000).unref();
 });
